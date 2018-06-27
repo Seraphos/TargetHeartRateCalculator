269cc80d8d0f7e1675de5922cae141f06fd3f124
Andrew Connelly <andrew.connelly@spgusa.com>
2018-06-27 14:52:27 -0400

    Add project files.

---
+++ TargetHeartRateCalculator/HeartRates.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace TargetHeartRateCalculator
+{
+    class HeartRates
+    {
+        private string firstName, lastName;
+        private int currentYear, yearOfBirth;
+        //firstname property
+        public string FirstName {
+            get { return firstName; }
+            set { firstName = value; } }
+        //lastname property
+        public string LastName {
+            get { return lastName; }
+            set { lastName = value; } }
+        //currentyear property; validate for negative numbers and zero
+        public int CurrentYear {
+            get { return currentYear; }
+            set { if (currentYear <= 0) throw new ArgumentOutOfRangeException("No Negative numbers!");
+                else currentYear = value; } }
+        //yearofbirth property; validate for negative numbers and zero
+        public int YearOfBirth {
+            get { return yearOfBirth; }
+            set { if (yearOfBirth <= 0) throw new ArgumentOutOfRangeException("No Negative numbers!");
+                else yearOfBirth = value; } }
+
+        public HeartRates(string constFirstName, string constLastName, int constCurrentYear, int constYearOfBirth)
+        {
+            firstName = constFirstName; lastName = constLastName; currentYear = constCurrentYear; yearOfBirth = constYearOfBirth;
+        }//end constructor
+
+        public double CalculateAge(int methCurrentYear, int methYearOfBirth)
+        {
+            double userAge = methCurrentYear - methYearOfBirth;
+            return userAge;              
+        }//end calculate age method
+
+        //this method depends on either asking the user their age or using the calculate age method
+        public double CalculateMaxumumHearRate(double methUserAge)
+        {
+            double userMaxHeartRate = 220-methUserAge;
+            return userMaxHeartRate;
+        }//end calculate max heart rate method
+
+        public double CalculateLowerTargetHeartRateRangeBound(double methMaxHeartRate)
+        {
+            double lowerTargetHeartRateBound;
+            lowerTargetHeartRateBound = methMaxHeartRate * .5;
+            return lowerTargetHeartRateBound;
+        }//end calc calculate lower boundary target heart rate range
+
+        public double CalculateUpperTargetHeartRateRangeBound(double methMaxHeartRate)
+        {
+            double upperTargetHeartRateBound;
+            upperTargetHeartRateBound = methMaxHeartRate * .85;
+            return upperTargetHeartRateBound;
+        }//end calculate upper boundary target heart rate range
+        
+    }//end class
+}//end namespace
---
+++ TargetHeartRateCalculator/TargetHeartRateCalculatorTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace TargetHeartRateCalculator
+{
+    class TargetHeartRateCalculatorTest
+    {
+        static void Main(string[] args)
+        {
+            int yearOfBirth, currentYear;
+            string firstName, lastName;
+            double maxHeartRate, userAge, lowerHRRangeBound, upperHRRangeBound;
+
+            Console.WriteLine("Enter your year of birth: ");
+            yearOfBirth = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the current year: ");
+            currentYear = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your first name: ");
+            firstName = Console.ReadLine();
+            Console.WriteLine("Enter your last name: ");
+            lastName = Console.ReadLine();
+
+            HeartRates newCustomerHeartRate = new HeartRates(firstName, lastName, currentYear, yearOfBirth);
+
+            userAge = newCustomerHeartRate.CalculateAge(currentYear, yearOfBirth);
+            maxHeartRate = newCustomerHeartRate.CalculateMaxumumHearRate(userAge);
+            lowerHRRangeBound = newCustomerHeartRate.CalculateLowerTargetHeartRateRangeBound(maxHeartRate);
+            upperHRRangeBound = newCustomerHeartRate.CalculateUpperTargetHeartRateRangeBound(maxHeartRate);
+
+            Console.WriteLine("{0} {1}, based on your input you are {2} years old.", firstName, lastName, userAge);
+            Console.WriteLine("Your maximum recommended heart rate is {0}", maxHeartRate);
+            Console.WriteLine("The ideal working heart rate range for you is between {0} bpm, and {1} bpm.", lowerHRRangeBound, upperHRRangeBound);
+
+
+
+        }//end main method
+    }//end class
+}//end namespace
